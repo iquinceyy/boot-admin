f5b2e16cc3903a089af090b8bb53c835d9be354a
quincey <973409978@qq.com>
2020-05-26 10:34:01 +0800

    5.26.09

---
+++ boot-pojo/src/main/java/com/qianfeng/boot/pojo/dto/ResponseDTO.java
@@ -0,0 +1,8 @@
+package com.qianfeng.boot.pojo.dto;
+
+/**
+ * Author quincey
+ * Date 2020/5/26 10:32
+ */
+public class ResponseDTO {
+}
---
+++ boot-pojo/src/main/java/com/qianfeng/boot/pojo/query/GoodsQuery.java
@@ -0,0 +1,8 @@
+package com.qianfeng.boot.pojo.query;
+
+/**
+ * Author quincey
+ * Date 2020/5/26 10:31
+ */
+public class GoodsQuery {
+}
